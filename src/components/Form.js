bd18eebb834b63db4387a8b55f888b4f2626d30c
Chris Curtis <chriscurtis.m@gmail.com>
2020-06-02 09:58:14 -0700

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,7 +9,7 @@ class Form extends Component{
 								
                         <input type="text" name="city" className="form-control mr-3 mb-3" placeholder="City.."/>
                                                 
-						<input type="text" name="state" className="form-control mr-3 mb-3" placeholder="state.."/>
+						<input type="text" name="country" className="form-control mr-3 mb-3" placeholder="Country.."/>
 				</div>
 				<div className="row justify-content-center">
 						<button type="submit" className="btn btn-info mb-3">Get Weather <i className="fas fa-arrow-alt-circle-right ml-1"></i></button>		
